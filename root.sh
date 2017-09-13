57ff8b3ab0f3d56896a49884fa802643049c25ad
Denis Andrejew <da.colonel@gmail.com>
2017-09-13 14:42:36 +0200

    adjusted to our current needs

--- root.sh
+++ root.sh
@@ -10,26 +10,17 @@ chmod 700 /home/$user/.ssh
 chmod 600 /home/$user/.ssh/authorized_keys
 chown -R $user:$user /home/$user/.ssh
 
-locale-gen ja_JP.UTF-8
-
-sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g" /etc/apt/sources.list
 apt-get update && apt-get -y upgrade
-apt-get -y install git ssh software-properties-common sudo tmux emacs-nox golang libssl-dev libreadline-dev zlib1g-dev global curl npm
-apt-add-repository -y ppa:fish-shell/release-2
-apt-get update
-apt-get -y install fish
-apt-get clean
-
-curl -O https://storage.googleapis.com/golang/$goversion.tar.gz
-tar -C /usr/local -xzf $goversion.tar.gz
-rm $goversion.tar.gz
+apt-get -y install git ssh software-properties-common npm wget curl
 
 npm i -g n
 npm i -g npm
-npm i -g eslint eslint-plugin-react
 n latest
 
-chsh -s /usr/bin/fish $user
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb
+apt-get update
+apt-get install -y esl-erlang elixir
+
 echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$user
 
 # sshd
--- setup.sh
+++ setup.sh
@@ -4,5 +4,3 @@ set -u
 source env.sh
 
 ./root.sh
-sudo -u $user -H ./setting.sh
-sudo -u $user -H ./user.sh
